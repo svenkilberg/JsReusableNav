f7c788ce24df8f5cfdacf9fedd463e9da77cf2e2
Sven Kilberg <sven.kilberg@gmail.com>
2020-07-31 06:05:27 +0200

    First commit

---
+++ Script/nav.js
@@ -0,0 +1,11 @@
+
+const navHtml = "<div id=\"navDiv\"><ul><li><a href=\"index.html\">Home</a></li><li><a href=\"page2.html\">Page 2</a></li><li><a href=\"page3.html\">Page 3</a></li></ul></div>";
+
+const navElement = document.querySelector("nav");
+
+navElement.innerHTML = navHtml;
+
+            
+            
+            
+        
