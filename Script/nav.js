797f6cade6f35126161faf584b83938390d11780
Sven Kilberg <sven.kilberg@gmail.com>
2020-08-17 06:13:09 +0200

    Another Link Added

--- Script/nav.js
+++ Script/nav.js
@@ -1,5 +1,5 @@
 
-const navHtml = "<div id=\"navDiv\"><ul><li><a href=\"index.html\">Home</a></li><li><a href=\"page2.html\">Page 2</a></li><li><a href=\"page3.html\">Page 3</a></li></ul></div>";
+const navHtml = "<div id=\"navDiv\"><ul><li><a href=\"index.html\">Home</a></li><li><a href=\"page2.html\">Page 2</a></li><li><a href=\"page3.html\">Page 3</a></li><li><a href=\"#\">Another Random Link</a></li></ul></div>";
 
 const navElement = document.querySelector("nav");
 
