51e25ecea73f509910c9c9242bb0228e6cf00791
Sven Kilberg <sven.kilberg@gmail.com>
2020-09-13 16:24:59 +0200

    Clean up of the navHtml markup

--- Script/nav.js
+++ Script/nav.js
@@ -1,9 +1,17 @@
 
-const navHtml = "<div id=\"navDiv\"><ul><li><a href=\"index.html\">Home</a></li><li><a href=\"page2.html\">Page 2</a></li><li><a href=\"page3.html\">Page 3</a></li><li><a href=\"#\">Another Random Link</a></li></ul></div>";
+const navHtml = '<nav>' + 
+                '<div id=\"navDiv\">' + 
+                '<ul>' + 
+                '<li><a href=\"index.html\">Home</a></li>' + 
+                '<li><a href=\"page2.html\">Page 2</a></li>' + 
+                '<li><a href=\"page3.html\">Page 3</a></li>' + 
+                '</ul>' + 
+                '</div>' + 
+                '</nav>';
 
-const navElement = document.querySelector("nav");
+const body = document.getElementsByTagName('body');
 
-navElement.innerHTML = navHtml;
+body[0].insertAdjacentHTML('afterbegin', navHtml);
 
             
             
